a787e23a2323378b7d053d896942685bfe0e8a67
theharism <chharis9999@gmail.com>
2024-08-08 03:47:46 +0500

    cal fix

--- utils/calculatePAS.js
+++ utils/calculatePAS.js
@@ -6,18 +6,18 @@ export function calculatePAS(
     appointmentsKept,
     salesSubmitted,
 ) {
-    const total_cases = Math.floor(salesTargets / averageCaseSize);
-    const cases_per_week = Math.floor(total_cases / numberOfWeeks);
+    const total_cases = salesTargets / averageCaseSize;
+    const cases_per_week = total_cases / numberOfWeeks;
 
     const weekly_goals = {
-        p_weekly: cases_per_week * prospectingApproach,
-        a_weekly: cases_per_week * appointmentsKept,
-        s_weekly: cases_per_week * salesSubmitted,
+        p_weekly: Math.round(cases_per_week * prospectingApproach),
+        a_weekly: Math.round(cases_per_week * appointmentsKept),
+        s_weekly: Math.round(cases_per_week * salesSubmitted),
     };
     const daily_goals = {
-        p_daily: Math.ceil(weekly_goals.p_weekly / 5),
-        a_daily: Math.ceil(weekly_goals.a_weekly / 5),
-        s_daily: Math.ceil(weekly_goals.s_weekly / 5),
+        p_daily: Math.round(weekly_goals.p_weekly / 5),
+        a_daily: Math.round(weekly_goals.a_weekly / 5),
+        s_daily: Math.round(weekly_goals.s_weekly / 5),
     };
 
     return { daily_goals, weekly_goals };
