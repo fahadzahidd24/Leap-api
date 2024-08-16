1ec618ebb5b930dd98f7330c8f223b19593797eb
theharism <chharis9999@gmail.com>
2024-08-16 21:24:21 +0500

    server change

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ import { Server } from "socket.io";
 const app = express();
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  path: "/location",
-});
+const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
   console.log(socket.id);
