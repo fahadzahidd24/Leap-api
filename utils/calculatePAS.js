d3d24b660ff95507b6e2599d3094fc894963a046
theharism <chharis9999@gmail.com>
2024-09-06 01:19:22 +0500

    weekly PAS fix

--- utils/calculatePAS.js
+++ utils/calculatePAS.js
@@ -1,24 +1,29 @@
 export function calculatePAS(
-    salesTargets,
-    averageCaseSize,
-    numberOfWeeks,
-    prospectingApproach,
-    appointmentsKept,
-    salesSubmitted,
+  salesTargets,
+  averageCaseSize,
+  numberOfWeeks,
+  prospectingApproach,
+  appointmentsKept,
+  salesSubmitted
 ) {
-    const total_cases = salesTargets / averageCaseSize;
-    const cases_per_week = total_cases / numberOfWeeks;
+  const total_cases = salesTargets / averageCaseSize;
+  const cases_per_week = total_cases / numberOfWeeks;
 
-    const weekly_goals = {
-        p_weekly: Math.ceil(cases_per_week * prospectingApproach),
-        a_weekly: Math.ceil(cases_per_week * appointmentsKept),
-        s_weekly: Math.ceil(cases_per_week * salesSubmitted),
-    };
-    const daily_goals = {
-        p_daily: Math.round(weekly_goals.p_weekly / 5),
-        a_daily: Math.round(weekly_goals.a_weekly / 5),
-        s_daily: Math.round(weekly_goals.s_weekly / 5),
-    };
+  const temp_daily = {
+    p_weekly: Math.ceil(cases_per_week * prospectingApproach),
+    a_weekly: Math.ceil(cases_per_week * appointmentsKept),
+    s_weekly: Math.ceil(cases_per_week * salesSubmitted),
+  };
+  const daily_goals = {
+    p_daily: Math.round(temp_daily.p_weekly / 5),
+    a_daily: Math.round(temp_daily.a_weekly / 5),
+    s_daily: Math.round(temp_daily.s_weekly / 5),
+  };
+  const weekly_goals = {
+    p_weekly: Math.round(daily_goals.p_daily * 5),
+    a_weekly: Math.round(daily_goals.a_daily * 5),
+    s_weekly: Math.round(daily_goals.s_daily * 5),
+  };
 
-    return { daily_goals, weekly_goals };
+  return { daily_goals, weekly_goals };
 }
