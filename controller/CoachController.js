3afbe1c20cbc987786f73b81976cf1d58cf08195
SoftwareDevelopersPro <75448023+SoftwareDevelopersPro@users.noreply.github.com>
2024-10-05 06:16:23 +0500

    Coach Module Done

--- controller/CoachController.js
+++ controller/CoachController.js
@@ -1,6 +1,7 @@
 import User from "../model/User.model.js";
 import jwt from "jsonwebtoken";
 import { stripPassword } from "./Auth.controller.js";
+import OpenAI from "openai";
 
 export const setProfession = async (req, res) => {
     try {
@@ -15,7 +16,7 @@ export const setProfession = async (req, res) => {
 
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
             expiresIn: "30d",
-          });
+        });
 
         return res.status(200).json({ success: true, user: { token, ...stripPassword(user._doc) } });
     } catch (error) {
@@ -23,3 +24,36 @@ export const setProfession = async (req, res) => {
         return res.status(400).json({ message: "Error setting profession", error });
     }
 }
+
+
+export const openAI = async (req, res) => {
+    try {
+        const { _id } = req.user;
+        const {prompt} = req.body;
+
+        const user = await User.findById(_id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+
+        const message = `${prompt}`
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: message }],
+        });
+
+        res.status(200).json({
+            message: response.choices[0].message,
+            prompt: message,
+        });
+    } catch (error) {
+        console.log("Error generating GPT respose", error);
+        return res.status(400).json({
+            message: "Internal Server Error",
+        });
+    }
+};
--- router/coach.route.js
+++ router/coach.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { setProfession } from '../controller/CoachController.js';
+import { setProfession, openAI} from '../controller/CoachController.js';
 const router = express.Router();
 
 router.post('/set-profession', setProfession);
+router.post('/ask-coach', openAI);
 
 export default router;
